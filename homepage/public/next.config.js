5094d86605a134f050f9a457e2fbf728bcdc45cb
hotaka0908 <ho@universalpine.com>
2025-05-21 15:44:26 +0900

    APIの変更

--- homepage/public/next.config.js
+++ homepage/public/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    OPENAI_API_KEY: process.env.openai_key, // 正しい環境変数名を参照
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY, // 正しい環境変数名を参照
   },
   // 静的ファイルの提供設定
   // 既存のHTMLファイルなどの静的ファイルを提供するための設定
